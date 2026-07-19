f18c95670b5c1d7ddf24af0f208ba2e8a3b498a1
Lee Kaburu <leekaburu@gmail.com>
2026-07-19 23:56:02 +0300

    feat(accounts): Made editting of the proile model from the admin panel easier by having all profile fields inline.

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import User, Profile
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
 
 
 class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
     fieldsets = BaseUserAdmin.fieldsets + (
-        (
-            "Verification",
-            {
-                "fields": (
-                    "is_email_verified",
-                    "otp_code_hash",
-                    "otp_expires_at",
-                )
-            },
-        ),
+        ("Verification", {"fields": ("is_email_verified", "otp_code_hash", "otp_expires_at")}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ("Required", {"fields": ("email",)}),
     )
     list_display = ("username", "email", "is_email_verified", "is_staff")
 
