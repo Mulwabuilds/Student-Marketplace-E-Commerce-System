40321bb1c40d32c3dfa202f9f216a34f100c8fd2
Lee Kaburu <leekaburu@gmail.com>
2026-07-21 11:45:43 +0300

    Simplify database configuration using DATABASE_URL

--- SMES/settings.py
+++ SMES/settings.py
@@ -24,14 +24,7 @@ DEBUG = env.bool("DEBUG", default=False)
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
-    }
+    "default": env.db("DATABASE_URL")
 }
 
 AUTH_USER_MODEL = "accounts.User"
